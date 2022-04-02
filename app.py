2bda22b9505a1705bc185a87cd16ecc55e169a6e
Evolone <evolonelayne@techexchange.in>
2022-04-02 20:09:53 +0000

    init commit - sign in/ log in stored and working just need css

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask import Flask
 from flask import render_template
 from flask import request, redirect, session, url_for
 from flask_pymongo import PyMongo
+import secrets
+# import bcrypt
 from organizations_info import organizations_info
 #from templates import education
 
@@ -23,6 +25,12 @@ mongo = PyMongo(app)
 # mongo.db.create_collection("organizations_info")
 # mongo.db.create_collection("user_info")
 
+secret_key = os.environ.get('MONGO_URI')
+# app.config['MONGO_URI'] = secret_key
+
+
+# -- Session data --
+app.secret_key = secrets.token_urlsafe(16)
 
 #Initialize PyMongo
 mongo = PyMongo(app)
@@ -33,7 +41,7 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
 
 
 @app.route('/addorg', methods=["GET", "POST"])
@@ -80,3 +88,66 @@ def tech():
     return render_template('tech.html', organizations = organizations)
 
 
+
+
+
+
+#SIGNUP Route
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == "POST":
+        users = mongo.db.users_info
+        #search for username in database
+        existing_user = users.find_one({'name': request.form['username']})
+
+        #if user not in database
+        if not existing_user:
+            username = request.form['username']
+            #encode password for hashing
+            password = request.form['password']
+            #hash password
+            # salt = bcrypt.gensalt()
+            # hashed = bcrypt.hashpw(password, salt)
+            #add new user to database
+            users.insert_one({'name': username, 'password': password})
+            #store username in session
+            session['username'] = request.form['username']
+            return redirect(url_for('signup'))
+
+        else:
+            return 'Username already registered.  Try logging in.'
+    
+    else:
+        return render_template('signup.html')
+
+#LOGIN Route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        users = mongo.db.users_info
+        #search for username in database
+        login_user = users.find_one({'name': request.form['username']})
+
+        #if username in database
+        if login_user:
+            db_password = login_user['password']
+            #encode password
+            password = request.form['password']
+            # compare username in database to username submitted in form
+            if password == db_password:
+                #store username in session
+                session['username'] = request.form['username']
+                return render_template('index.html')
+            else:
+                return 'Invalid username/password combination.'
+        else:
+            return 'User not found.'
+    else:
+        return render_template('login.html')
+    
+#LOGOUT Route
+@app.route('/logout')
+def logout():
+    #clear username from session data
+    session.clear()
+    return redirect('/')
